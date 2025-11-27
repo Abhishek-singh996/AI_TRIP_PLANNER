3c334c506f6152b0829ab7aedf30de6245fc14c5
Abhishek-singh996 <nitianabhishek996@gmail.com>
2025-11-27 08:59:51 +0000

    agent workflow created

---
+++ agent/agent_workflow.py
@@ -0,0 +1,16 @@
+from utils.model_loader import Model_loader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import Graph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+class GraphBuilder:
+    def __init__(self):
+        pass
+    def agent_function(self):
+        pass
+    
+    def build_graph(self):
+        pass
+    
+    
